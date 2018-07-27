a8146a06e00b0995be294a6db68a7552f3787c9d
Julien Palard <julien@palard.fr>
2018-07-27 15:55:19 +0200

    setup.py: Add dev dependencies.

--- setup.py
+++ setup.py
@@ -2,34 +2,33 @@
 
 import setuptools
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     long_description = readme.read()
 
 setuptools.setup(
-    name='pospell',
-    version='0.1.1',
+    name="pospell",
+    version="0.1.1",
     description="Spellcheck .po files containing reStructuredText translations",
     long_description=long_description,
-    long_description_content_type='text/markdown', # This is important!
+    long_description_content_type="text/markdown",  # This is important!
     author="Julien Palard",
-    author_email='julien@palard.fr',
-    url='https://github.com/JulienPalard/pospell',
-    py_modules=[
-        'pospell',
-    ],
-    entry_points={
-        'console_scripts': ['pospell=pospell:main']
+    author_email="julien@palard.fr",
+    url="https://github.com/JulienPalard/pospell",
+    py_modules=["pospell"],
+    entry_points={"console_scripts": ["pospell=pospell:main"]},
+    extras_require={
+        "dev": ["bandit", "black", "detox", "flake8", "isort", "mypy", "pylint"]
     },
-    install_requires=['polib'],
+    install_requires=["polib"],
     license="MIT license",
-    keywords='po spell gettext reStructuredText check sphinx translation',
+    keywords="po spell gettext reStructuredText check sphinx translation",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
