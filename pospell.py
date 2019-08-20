079c7361510b16556eb47adc105a8cbe402bf4a9
Julien Palard <julien@palard.fr>
2019-08-20 16:38:07 +0200

    Bump version: 0.3.5 → 0.3.6

--- pospell.py
+++ pospell.py
@@ -18,7 +18,7 @@ from docutils.utils import new_document
 
 import regex
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 try:
     HUNSPELL_VERSION = subprocess.check_output(
         ["hunspell", "--version"], universal_newlines=True
