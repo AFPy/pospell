680433adfda5f16c540fb85d63a25ab1cf848da9
Julien Palard <julien@palard.fr>
2020-07-01 17:38:42 +0200

    Bump version: 1.0.4 → 1.0.5

--- pospell.py
+++ pospell.py
@@ -20,7 +20,7 @@ from docutils.utils import new_document
 
 import regex
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 DEFAULT_DROP_CAPITALIZED = {"fr": True, "fr_FR": True}
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as readme:
 
 setuptools.setup(
     name="pospell",
-    version="1.0.4",
+    version="1.0.5",
     description="Spellcheck .po files containing reStructuredText translations",
     long_description=long_description,
     long_description_content_type="text/markdown",  # This is important!
